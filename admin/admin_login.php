af1eb10f15cfbfc9c140b8897f1dcad7291bd073
Audrey Obuobisa-Darko <audrey.obuobisa@ashesi.edu.gh>
2022-11-11 14:21:19 +0000

    ad log

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -38,67 +38,21 @@
 </head>
 
 <body>
-<p>hi</p>
-<main id="main">
-
-    <!-- ======= Login Section ======= -->
-    <section id="contact" class="contact">
-      <div class="container">
-
-        <div class="section-title" data-aos="zoom-out">
-          <h2>Contact</h2>
-          <p>Contact Us</p>
-        </div>
-
-        <div class="row mt-5">
-
-          <div class="col-lg-4" data-aos="fade-right">
-            <div class="info">
-              <div class="address">
-                <i class="bi bi-geo-alt"></i>
-                <h4>Location:</h4>
-                <p>A108 Adam Street, New York, NY 535022</p>
-              </div>
-
-              <div class="email">
-                <i class="bi bi-envelope"></i>
-                <h4>Email:</h4>
-                <p>info@example.com</p>
-              </div>
-
-              <div class="phone">
-                <i class="bi bi-phone"></i>
-                <h4>Call:</h4>
-                <p>+1 5589 55488 55s</p>
-              </div>
-
+    
+    <form action="../actions/admin_action.php" method="post" role="form" class="php-email-form">
+        <div class="row">
+             <div class="col-md-6 form-group mt-3 mt-md-0">
+                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
             </div>
-
-          </div>
-
-          <div class="col-lg-8 mt-5 mt-lg-0" data-aos="fade-left">
-
-            <form action="../actions/admin_action.php" method="post" role="form" class="php-email-form">
-              <div class="row">
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-                </div>
-              </div>
-              <div class="form-group mt-3">
-                <input type="password" class="form-control" name="password" id="subject" placeholder="Password" required>
-              </div>
-              
-              <div class="text-center"><button type="submit">Login</button></div>
-            </form>
-
-          </div>
-
         </div>
+        <div class="form-group mt-3">
+            <input type="password" class="form-control" name="password" id="subject" placeholder="Password" required>
+        </div>
+        <div class="text-center"><button type="submit">Login</button></div>
+    </form>
 
-      </div>
-    </section><!-- End Contact Section -->
+    
 
-  </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
   <footer id="footer">
