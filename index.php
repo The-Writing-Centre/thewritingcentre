ec74c2f00eb80e3d512d91cb46fefd18999b3807
Ndaale Ansah <ndaaleansah80@gmail.com>
2022-11-14 21:47:47 +0000

    index

--- index.php
+++ index.php
@@ -72,9 +72,6 @@
               <li><a href="#">Drop Down 4</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="nav-link scrollto" href="admin/index.php">Audrey Admin Test</a></li>
-          <li><a class="nav-link scrollto" href="admin/admin_login.php">Audrey Admin Login Test</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
@@ -89,7 +86,7 @@
       <!-- Slide 1 -->
       <div class="carousel-item active">
         <div class="carousel-container">
-          <h2 class="animate__animated animate__fadeInDown">Welcome to <span>Selecao</span></h2>
+          <h2 class="animate__animated animate__fadeInDown">Welcome to <span>The Writing Center</span></h2>
           <p class="animate__animated fanimate__adeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
           <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
         </div>
