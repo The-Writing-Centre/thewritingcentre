8f8d0c38c7354adbf765b333c4e969bce44676bb
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-11-19 19:27:40 +0000

    chale

--- view/signin_process.php
+++ view/signin_process.php
@@ -5,31 +5,32 @@ require('../Controllers/user_controller.php');
 
 //start session
 session_start();
+echo ("Hello");
+// if(isset($_POST["sign_in"]))
+// { 
 
-if (isset($_POST["sign_in"])) {
-    echo ("hello");
-    // $email = $_POST["email"];
-    // $password = $_POST["password"];
-
-
-    // $result = check_login_details_controller($email);
-
-    // if(isset($result["email"])){
-    //     password_verify($password, $result["password"]);
-    //     $_SESSION["user_id"] = $result["user_id"];
-    //     $_SESSION["user_role"] = $result["role"];
-
-    //     if($_SESSION["user_role"] == 2){
-    //         header('location: ../admin/admin_login.php');
-    //     }
-    //    if($_SESSION["user_role"] == 2){
-    //     // header('location: success.php');
-    //     echo header('location: ../portfolio-details.php');
-    //    }
-
-    // }
-    // else echo header('location: ../portfolio-details.php');
+//     $email = $_POST["email"];
+//     $password = $_POST["password"];
 
 
+//     $result = check_login_details_controller($email);
 
-}
+//     if(isset($result["email"])){
+//         password_verify($password, $result["password"]);
+//         $_SESSION["user_id"] = $result["user_id"];
+//         $_SESSION["user_role"] = $result["role"];
+
+//         if($_SESSION["user_role"] == 2){
+//             header('location: ../admin/admin_login.php');
+//         }
+//        if($_SESSION["user_role"] == 2){
+//         // header('location: success.php');
+//         echo header('location: ../portfolio-details.php');
+//        }
+
+//     }
+//     else echo header('location: ../portfolio-details.php');
+    
+    
+    
+// }
