aef4e98d93fc058f4b170c133dc46b6fc577d5dc
Ndaale Ansah <ndaaleansah80@gmail.com>
2022-11-19 01:53:54 +0000

    Update signin_process.php

---
+++ view/signin_process.php
@@ -0,0 +1,37 @@
+<?php
+
+
+require('../Controllers/user_controller.php');
+
+//start session
+session_start();
+
+if(isset($_POST["sign_in"]))
+{ 
+
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+
+    $result = check_login_details_controller($email);
+
+    if(isset($result["email"])){
+        password_verify($password, $result["password"]);
+        $_SESSION["user_id"] = $result["user_id"];
+        $_SESSION["user_role"] = $result["user_role"];
+
+        if($_SESSION["user_role"] == 1){
+            header('location: ../admin');
+        }
+       if($_SESSION["user_role"] == 2){
+        header('location: success.php');
+       }
+
+    }
+    else echo header('location: ../portfolio');
+    
+    
+    
+}
+
+?>
