1bec7ae32ce6bf370bfd7251b7dab36d20ee811c
Ndaale Ansah <ndaaleansah80@gmail.com>
2022-11-19 01:32:39 +0000

    controller

--- controllers/user_controller.php
+++ controllers/user_controller.php
@@ -3,85 +3,56 @@ require('../classes/user_class.php');
 //Add user controller
 function add_user_controller($name, $email, $password, $country, $city, $contact)
 {
-    //create user
- instance
-    $user
-_instance = new user
-();
+    //create user instance
+    $user_instance = new Customer();
     //call the method from the class
-    return $user
-_instance->add_cust($name, $email, $password, $country, $city, $contact);
+    return $user_instance->add_user($name, $email, $password, $country, $city, $contact);
 }
 
-//Edit user controller
+//Edit use controller
 function edit_user_controller($id, $name, $email, $password, $country, $city, $contact)
 {
-    //create user
- instance
-    $user
-_instance = new user
-();
+    //create user instance
+    $user_instance = new Customer();
     //call the method from the class
-    return $user
-_instance->edit_cust($id, $name, $email, $password, $country, $city, $contact);
+    return $user_instance->edit_user($id, $name, $email, $password, $country, $city, $contact);
 
 }
 
 //Verify Login controller
 function check_login_details_controller($email)
 {
-    //create user
- instance
-    $user
-_instance = new user
-();
+    //create user instance
+    $user_instance = new Customer();
     //call the method from the class
-    return $user
-_instance->check_login_details($email);
+    return $user_instance->check_login_details($email);
 }
 
 
-//Delete user controller
-function delete_user_controller($id)
+//Delete use controller
+function delete_use_controller($id)
 {
-    //create user
- instance
-    $user
-_instance = new user
-();
-    return $user
-_instance->delete_cust($id);
+    //create user instance
+    $user_instance = new Customer();
+    return $user_instance->delete_user($id);
 }
 
-function select_all_user_controller(){
-    // create an instance of the user
- class
-    $user
-_instance = new user
-();
+function select_all_use_controller(){
+    // create an instance of the user class
+    $user_instance = new Customer();
     // call the method from the class
-    return $user
-_instance->select_all_cust();
+    return $user_instance->select_all_users();
 
 }
 
-function select_one_user_controller($id){
-    // create an instance of the user
- class
-    $user
-_instance = new user
-();
+function select_one_use_controller($id){
+    // create an instance of the user class
+    $user_instance = new Customer();
     // call the method from the class
-    return $user
-_instance->select_one_cust($id);
+    return $user_instance->select_one_user($id);
 
 
 }
-
-// function sign_up_controller($name, $email, $password){
-//  $new_person = new user();
-//  return $new_person->sign_up($name, $email, $password);
-// }
 
 
 
