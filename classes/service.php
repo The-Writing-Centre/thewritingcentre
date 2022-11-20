1e11cd895167fc1e18314550f19baa2d0eb21b6f
ErnestABenefo <60148334+ErnestABenefo@users.noreply.github.com>
2022-11-20 19:02:41 +0000

    Add files via upload
    Add service form.

---
+++ classes/service.php
@@ -0,0 +1,27 @@
+<?php
+    require('../Settings/core.php');
+    require('../Controllers/product_controller.php');
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="../actions/add_service.php" method="POST">
+        <input type="text" name="service_name" placeholder="service name">
+        <!-- <input type="text" name="service_cat" placeholder="service category"> -->
+        <input type="text" name="service_desc" placeholder="description">
+        <input type="text" name="service_price" placeholder="price">
+        <!-- <input type="file" name="service_image" placeholder="upload file"> -->
+        <input type="text" name="service_keywords" placeholder="keywords">
+        <!-- <input type="date" id="start" name="ddate"> -->
+        <button type="submit" name="submit">Sign up</button>
+    </form>
+</body>
+</html>
