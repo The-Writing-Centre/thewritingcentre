571974fafc32292f03ee7f3553a44b5ba69de59f
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-11-23 15:50:21 +0000

    cust edit

--- admin/customers.php
+++ admin/customers.php
@@ -1,8 +1,8 @@
 <?php
-    include ("../settings/core.php");
-    require_once("../controllers/user_controller.php");
-
-    $users = select_one_use_controller($user_id);
+include("../settings/core.php");
+require_once("../controllers/user_controller.php");
+$user_id = $_SESSION['user_id'];
+$users = select_one_use_controller($user_id);
 
 ?>
 <!DOCTYPE html>
@@ -22,7 +22,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&display=swap" rel="stylesheet">
-    
+
     <!-- Icon Font Stylesheet -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
@@ -55,7 +55,7 @@
                 <a href="index.php" class="navbar-brand mx-4 mb-3">
                     <h1 class="text-primary"></i>TWC</h1>
                 </a>
-                
+
                 <div class="navbar-nav w-100">
                     <a href="index.php" class="nav-item nav-link"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
                 </div>
@@ -77,9 +77,9 @@
                 <a href="#" class="sidebar-toggler flex-shrink-0">
                     <i class="fa fa-bars"></i>
                 </a>
-            
+
                 <div class="navbar-nav align-items-center ms-auto">
-                    
+
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                             <span class="d-none d-lg-inline-flex">Admin</span>
@@ -115,29 +115,27 @@
                             </thead>
 
                             <tbody>
-                            <!--fetching and displaying orders-->
-                            <?php
-                            if(!empty($users)){
-                                foreach($users as $user){
-                                    ?><tr>
-                                        <td><?=$user['user_id']?></td>
-                                        <td><?=$user['first_name']?></td>
-                                        <td><?=$user['last_name']?></td>
-                                        <td><?=$user['school']?></td>
-                                        <td><?=$user['programme']?></td>
-                                        <td><?=$user['email']?></td> 
-                                        <td><?=$user['contact']?></td>                                  
-                                 
-                                        
-                                    </tr>
-                                    <?php 
-                               }
-                            }
+                                <!--fetching and displaying orders-->
+                                <?php
+                                if (!empty($users)) {
+                                    foreach ($users as $user) {
+                                ?><tr>
+                                            <td><?= $user['user_id'] ?></td>
+                                            <td><?= $user['first_name'] ?></td>
+                                            <td><?= $user['last_name'] ?></td>
+                                            <td><?= $user['school'] ?></td>
+                                            <td><?= $user['programme'] ?></td>
+                                            <td><?= $user['email'] ?></td>
+                                            <td><?= $user['contact'] ?></td>
 
-                            else{
-                                echo "<tr><td>No Customers</td></tr>";
-                            }
-                            ?>  
+
+                                        </tr>
+                                <?php
+                                    }
+                                } else {
+                                    echo "<tr><td>No Customers</td></tr>";
+                                }
+                                ?>
                             </tbody>
                         </table>
                     </div>
@@ -153,13 +151,13 @@
                 <div class="bg-light rounded-top p-4">
                     <div class="row">
                         <div class="col-12 col-sm-6 text-center text-sm-start">
-                            &copy; <a href="#">The Writing Centre</a>, All Rights Reserved. 
+                            &copy; <a href="#">The Writing Centre</a>, All Rights Reserved.
                         </div>
                         <div class="col-12 col-sm-6 text-center text-sm-end">
                             <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
                             Designed By <a href="https://htmlcodex.com">HTML Codex</a>
-                        </br>
-                        Distributed By <a class="border-bottom" href="https://themewagon.com" target="_blank">ThemeWagon</a>
+                            </br>
+                            Distributed By <a class="border-bottom" href="https://themewagon.com" target="_blank">ThemeWagon</a>
                         </div>
                     </div>
                 </div>
