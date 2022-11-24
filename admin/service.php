b33b1e0b4c2d5292639855ee1c4553316519ff5c
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-11-24 00:24:09 +0000

    service(bad)

--- admin/service.php
+++ admin/service.php
@@ -1,17 +1,19 @@
 <?php
-    require('../Settings/core.php');
-    require('../Controllers/product_controller.php');
-    
+require('../Settings/core.php');
+require('../Controllers/product_controller.php');
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Add Services</title>
 </head>
+
 <body>
     <form action="../actions/add_service.php" method="POST">
         <input type="text" name="service_name" placeholder="service name">
@@ -21,7 +23,9 @@
         <!-- <input type="file" name="service_image" placeholder="upload file"> -->
         <input type="text" name="service_keywords" placeholder="keywords">
         <!-- <input type="date" id="start" name="ddate"> -->
+        <input type="file" name="service_image" id="service_image">
         <button type="submit" name="submit">Sign up</button>
     </form>
 </body>
+
 </html>
