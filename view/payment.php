66837e3929cce85ca6a017c74a70e50b23fb7fe5
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-11-24 09:46:44 +0000

    payment is broken

--- view/payment.php
+++ view/payment.php
@@ -4,8 +4,8 @@ require('../settings/core.php');
 require('../controllers/cart_controller.php');
 check_login();
 
-$customer_id= $_SESSION['user_id'];
-$email= $_SESSION['customer_email'];
+$customer_id = $_SESSION['user_id'];
+$email = $_SESSION['customer_email'];
 $amount = total_amount_controller($customer_id);
 
 ?>
@@ -28,12 +28,12 @@ $amount = total_amount_controller($customer_id);
             <div class="row">
                 <div class="col-md-12">
                     <h2 class="mt-5">Customer Payment</h2>
-                    <p>Please fill this form to make payment to the application.</p>
+                    <!-- <p>Please fill this form to make payment to the application.</p> -->
                     <!-- Login Form -->
                     <form action="../js/payment.js" method="POST" id="paymentForm">
 
                         <!-- Name Input Section -->
-                        <section>
+                        <!-- <section>
                             <div class="row">
                                 <div class="col">
                                     <input type="text" class="form-control" placeholder="First name" name="fname" id="fname">
@@ -43,21 +43,21 @@ $amount = total_amount_controller($customer_id);
                                 </div>
                             </div>
 
-                        </section>
+                        </section> -->
 
                         <!-- Email field -->
-                        <div class="form-group">
+                        <!-- <div class="form-group">
                             <label>Email</label>
                             <input name="email" class="form-control" placeholder="Enter email..." required></textarea>
                             <span class="invalid-feedback"></span>
-                        </div>
+                        </div> -->
 
                         <!-- Password field -->
-                        <div class="form-group">
+                        <!-- <div class="form-group">
                             <label>Password</label>
                             <input name="password" class="form-control" type="password" placeholder="Enter password..." required></textarea>
                             <span class="invalid-feedback"></span>
-                        </div>
+                        </div> -->
 
                         <!-- <div class="form-group">
                             <label for="service">Service</label>
@@ -69,7 +69,7 @@ $amount = total_amount_controller($customer_id);
                         </div> -->
 
 
-                        <div class="form-group">
+                        <!-- <div class="form-group">
                             <label for="service">Service Select</label>
                             <select class="custom-select" name="service" id="service">
                                 <option selected>Select one</option>
@@ -77,14 +77,14 @@ $amount = total_amount_controller($customer_id);
                                 <option value="4">Cover Letter Writing</option>
                                 <option value="5">Graduate Essay Writing</option>
                             </select>
-                        </div>
+                        </div> -->
 
-                        <div class="form-group">
+                        <!-- <div class="form-group">
                           <label for="number">Number of Selection</label>
                           <input type="number"
                             class="form-control" name="number" id="number" aria-describedby="helpId" placeholder="Enter a number" min="1" step="1">
                           <small id="helpId" class="form-text text-muted">Help text</small>
-                        </div>
+                        </div> -->
 
                         <!-- Submit & cancel buttons -->
                         <!-- <input type="submit" class="btn btn-primary" value="Submit" name="login"> -->
@@ -104,7 +104,42 @@ $amount = total_amount_controller($customer_id);
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="../js/payment.js"></script>
+    <!-- <script src="../js/payment.js"></script> -->
+    <script src="https://js.paystack.co/v1/inline.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script>
+        < script >
+            const paymentForm = document.getElementById('paymentForm');
+        paymentForm.addEventListener("submit", payWithPaystack, false);
+
+        // PAYMENT FUNCTION
+        function payWithPaystack(e) {
+            e.preventDefault();
+            let handler = PaystackPop.setup({
+                key: 'pk_live_bd5356607a881f3a0d6843b75d3172b74b9675cd', // Replace with your public key
+                email: document.getElementById("email-address").value,
+                amount: document.getElementById("amount").value * 100,
+                currency: 'GHS',
+                onClose: function() {
+                    alert('Window closed.');
+                },
+                callback: function(response) {
+                    window.location = `../actions/processing.php?email=${document.getElementById("email-address").value}&amount=${document.getElementById("amount").value}&reference=${response.reference}`
+                    // alert("payment have been made"+ response.reference);
+                    // $.ajax({
+                    //   url:`../actions/processing?email=${document.getElementById("email-address").value}&amount=${document.getElementById("amount").value}&reference=${response.reference}`,
+                    //   method:'GET',
+                    //   success: function (response){
+                    //     alert(response);
+                }
+
+
+            });
+            //}
+            // });
+            handler.openIframe();
+        }
+    </script>
 </body>
 
 
